a93b7af2b10ae9275d43b44fa5c48dadac4cf956
OMAR-R3 <joseomargarciacruz@gamil.com>
2025-03-14 21:26:19 -0600

    eliminacion de funcion obsoleta

--- middlewares/funcionesPassword.js
+++ middlewares/funcionesPassword.js
@@ -19,15 +19,6 @@ export function validarPassword(password, salt, hash) {
     return hashEvaluar == hash;
 }
 
-/*export function usuarioAutorizado(token, req) {
-    if (!token) return mensaje(400, "Usuario no autorizado");
-    jwt.verify(token, process.env.SECRET_TOKEN, (error, usuario) => {
-        if (error) return mensaje(400, "Usuario no autorizado");
-        req.usuario = usuario;
-    });
-    return mensaje(200, "Usuario autorizado");
-}*/
-
 export async function usuarioAutorizado(token, req) {
     if (!token) return mensaje(400, "Usuario no autorizado");
 
@@ -41,47 +32,16 @@ export async function usuarioAutorizado(token, req) {
 
 
 
-/*export async function adminAutorizado(req) {
-    const respuesta = usuarioAutorizado(req.cookies.token, req);
-    if (respuesta.status != 200) {
-        return mensaje(400, "1 Admin no autorizado");
-    }
-    if (await isAdmin(req.usuario.id) == false) {
-        return mensaje(400, " 2Admin no autorizado");
-    }
-    return mensaje(200, "3 Admin autorizado");
-}*/
 
 export async function adminAutorizado(req) {
-    //console.log("Cookies recibidas en la solicitud:", req.cookies);
-
     const respuesta = await usuarioAutorizado(req.cookies.token, req);
-
     if (respuesta.status !== 200) {
         return mensaje(400, "1 Admin no autorizado");
     }
-
-    //console.log("Usuario en la solicitud:", req.usuario);
-
     const esAdmin = await showId(req.usuario.id);
-
-    //console.log("Resultado de isAdmin:", esAdmin);
-
     if (!esAdmin.tipoUsuario) {
         return mensaje(400, "2 Admin no autorizado");
     }
 
     return mensaje(200, "3 Admin autorizado");
 }
-
-export async function Hola() {
-    console.log("Hola Omar");
-}
-
-
-/*const {salt,hash} = encriptarPassword("abc");
-console.log("salt ------> "+salt);
-console.log("hash ------> "+hash);
-
-const aprobado = validarPassword("abc",salt,hash);
-console.log("Resultado: "+aprobado);*/
