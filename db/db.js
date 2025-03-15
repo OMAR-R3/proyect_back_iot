6dc850da0a57980735b9c41d28a9c4a03ebe2757
Cesar0795 <eduardoef.ti23@utsjr.edu.mx>
2025-03-14 21:52:36 -0600

    Cambio de base de datos a MongoDB Atlas y eliminacion de comentarios innecesarios

--- db/db.js
+++ db/db.js
@@ -4,10 +4,6 @@ import {mensaje} from "../libs/mensajes.js"
 export async function conectarDB() {
     try {
         const conexion = await mongoose.connect("mongodb+srv://Cesar:ce07102005@cluster0.rrfpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-        //const conexion = await mongoose.connect("mongodb://127.0.0.1:27017/MongoApps");//faltantes
-        //const conexion2= await mongoose.connection("mongodb://127.0.0.1:27017/MongoUbicaciones");//faltantes
-        //console.log(conexion);
-        //console.log("Conexion correcta a mongodb");
         return mensaje(200,"conexion extosa","");
     } catch (error) {
         return mensaje(400,"error al conectarse a la bd", error);
