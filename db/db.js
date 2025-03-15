d63bd9b3ea426dbd5fcc1f45959ad45881e59f50
Cesar0795 <eduardoef.ti23@utsjr.edu.mx>
2025-03-15 13:40:07 -0600

    Cambio de ruta en acceso a base de datos en MongoDB Atlas

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import {mensaje} from "../libs/mensajes.js"
 
 export async function conectarDB() {
     try {
-        const conexion = await mongoose.connect("mongodb+srv://Cesar:ce07102005@cluster0.rrfpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const conexion = await mongoose.connect("mongodb+srv://ka1z3n65:ka1z3n65@kaizen.q5t72.mongodb.net/?retryWrites=true&w=majority&appName=Kaizen");
         return mensaje(200,"conexion extosa","");
     } catch (error) {
         return mensaje(400,"error al conectarse a la bd", error);
