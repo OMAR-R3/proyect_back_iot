8196f29ca39ed1beff73dd128957e5f72cce1d22
OMAR-R3 <joseomargarciacruz@gamil.com>
2025-03-15 23:49:41 -0600

    creacion de funciones de correo

--- db/usuariosDB.js
+++ db/usuariosDB.js
@@ -80,6 +80,7 @@ const enviarCorreoRegistro = async (email, username, password) => {
 };
 
 
+
 /*export const crearUsuario = async ({ username, email, password }) => {
     try {
         const usuarioDuplicado = await User.findOne({ username });
@@ -110,7 +111,7 @@ export const login = async ({ email, password }) => {
             sonName: usuarioEncontrado.sonName,
             email: usuarioEncontrado.email
         });
-        return mensaje(200, usuarioEncontrado.tipoUsuario, "", "", token);
+        return mensaje(200, "Logueado con exito", "", "", token);
     } catch (error) {
         return mensaje(400, "error al logearse", error);
     }
@@ -140,38 +141,45 @@ export const showId = async (_id) => {
 
 export const deleteId = async (_id) => {
     try {
+
         const usuarioEncontrado = await User.findOne({ _id });
         if (!usuarioEncontrado) { return mensaje(400, "usuario no encontrado") }
 
         const usuarioEliminado = await User.findByIdAndDelete({ _id });
         if (!usuarioEliminado) { return mensaje(400, "usuario no eliminado") }
+
+        await enviarCorreoDelete(usuarioEncontrado.email);
+        console.log("1");
+        
         return mensaje(200, `Usuario ${usuarioEncontrado.username} eliminado correctamente`);
     } catch (error) {
         return mensaje(400, "error al buscar usuario", error);
     }
 }
 
-export const updateId = async ({ _id, sonName, email, password, tipoUsuario }) => {
+export const updateId = async ({ _id, sonName, email, password}) => {
     try {
         const usuarioEncontrado = await User.findOne({ _id });
         if (!usuarioEncontrado) {
             return mensaje(400, "Usuario no encontrado");
         }
-        if (usuarioEncontrado.username !== username) {
-            const usuarioDuplicado = await User.findOne({ username });
-            if (usuarioDuplicado) { return mensaje(400, "nombre de usuario existente") };
-        };
         if (usuarioEncontrado.email !== email) {
             const emailDuplicado = await User.findOne({ email });
             if (emailDuplicado) { return mensaje(400, "email de usuario existente") };
         };
 
+        const passwordOriginal = password;
+
+        await enviarCorreoUpdate(email, sonName, passwordOriginal);
+
         const { salt, hash } = encriptarPassword(password);
-        const dataUser = { sonName, email, password: hash, tipoUsuario, salt };
+        const dataUser = { sonName, email, password: hash, salt };
 
         const usuarioActualizado = await User.findByIdAndUpdate(_id, dataUser, { new: true });//{ new: true } para que MongoDB devuelva el documento actualizado, no el original.
 
         if (!usuarioActualizado) { return mensaje(400, "usuario no actualizado") }
+
+
 
         return mensaje(200, "Usuario actualizado correctamente");
     } catch (error) {
@@ -179,6 +187,78 @@ export const updateId = async ({ _id, sonName, email, password, tipoUsuario }) =
         return mensaje(400, "error al intentar actualizar datos", error);
     }
 }
+
+const enviarCorreoUpdate= async (email, sonName, password) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "ka1z3n65@gmail.com",
+            pass: "dsewwbkgjkckefuo"
+        }
+    });
+
+    const datos = JSON.stringify({ email, password });
+    const qrCode = await QRCode.toDataURL(datos);
+    const imgBuffer = Buffer.from(qrCode.split(",")[1], 'base64');
+
+    const mailOptions = {
+        from: "ka1z3n65@gmail.com",
+        to: email,
+        subject: "Actualización exitosa",
+        html: `
+            <p>Hola,</p>
+            <p>Su cuenta ha sido actualizada con éxito.</p>
+            <p><strong>Nombre de su hij@:</strong> ${sonName}</p>
+            <p><strong>Su nueva es contraseña:</strong> ${password}</p>
+            <p>Su nuevo QR es:</p>
+            <img src="cid:qrcode" alt="Código QR" style="width: 150px; height: 150px;" />
+            <p>Saludos,<br>Equipo de SNAPI</p>
+        `,
+        attachments: [
+            {
+                filename: 'qrcode.png',
+                content: imgBuffer,
+                cid: 'qrcode'
+            }
+        ]
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.log("Error al enviar el correo:", error);
+    }
+};
+
+
+const enviarCorreoDelete= async (email) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "ka1z3n65@gmail.com",
+            pass: "dsewwbkgjkckefuo"
+        }
+    });
+    const mailOptions = {
+        from: "ka1z3n65@gmail.com",
+        to: email,
+        subject: "Aviso de eliminacion",
+        html: `
+            <p>Hola que tal,</p>
+            <p>Su cuenta ha sido eliminada con exito.</p>
+            <p><strong>Si cree que fue un error favor de comunicarse con nosotros.</p>
+            <p><strong>Correo para aclaración de dudas: ka1z3n65@gmail.com</p>
+            <p>Saludos,<br>Equipo de SNAPI</p>
+        `
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.log("Error al enviar el correo:", error);
+    }
+};
+
 
 /*export const isAdmin = async (id) => {
     try {
@@ -259,10 +339,10 @@ export const registerAdmin = async ({ username, email, password }) => {
         //enviar correo
         await enviarCorreoRegistroAdmin(email, username, passwordOriginal);
 
-        return mensaje(200, "Usuario registrado con exito", "", "", token);
+        return mensaje(200, "Administrador registrado con exito", "", "", token);
 
     } catch (error) {
-        return mensaje(400, "error usuario no registrado", error);
+        return mensaje(400, "error administrador no registrado", error);
     }
 };
 
@@ -357,7 +437,7 @@ export const deleteIdAdmin = async (_id) => {
         if (!adminEliminado) { return mensaje(400, "admin no eliminado") }
         return mensaje(200, `Administrador ${adminEncontrado.username} eliminado correctamente`);
     } catch (error) {
-        return mensaje(400, "error al buscar usuario", error);
+        return mensaje(400, "error al buscar administrador", error);
     }
 }
 
@@ -369,11 +449,11 @@ export const updateIdAdmin = async ({ _id, email, password}) => {
         }
         if (adminEncontrado.username !== username) {
             const adminDuplicado = await Admin.findOne({ username });
-            if (adminDuplicado) { return mensaje(400, "nombre de usuario existente") };
+            if (adminDuplicado) { return mensaje(400, "nombre de administrador existente") };
         };
         if (adminEncontrado.email !== email) {
             const emailDuplicado = await Admin.findOne({ email });
-            if (emailDuplicado) { return mensaje(400, "email de usuario existente") };
+            if (emailDuplicado) { return mensaje(400, "email de administrador existente") };
         };
 
         const { salt, hash } = encriptarPassword(password);
