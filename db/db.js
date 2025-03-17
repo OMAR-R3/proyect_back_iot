885c7c4a694499090e8be08d919c3ba1fe97b2ab
OMAR-R3 <joseomargarciacruz@gamil.com>
2025-03-17 00:47:10 -0600

    comentarios extras

--- db/db.js
+++ db/db.js
@@ -3,9 +3,12 @@ import {mensaje} from "../libs/mensajes.js"
 
 export async function conectarDB() {
     try {
+        //Conexion a mongo Atlas
         const conexion = await mongoose.connect("mongodb+srv://ka1z3n65:ka1z3n65@kaizen.q5t72.mongodb.net/?retryWrites=true&w=majority&appName=Kaizen");
+        //retorno de mensaje de conexion exitosa
         return mensaje(200,"conexion extosa","");
     } catch (error) {
+        //Manejo de errores
         return mensaje(400,"error al conectarse a la bd", error);
     }   
 }
