6d1ad537a491bec0932cbb2cdab9cdfce671f567
OMAR-R3 <joseomargarciacruz@gamil.com>
2025-03-18 10:23:26 -0600

    protocolo https

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import https from "https";
+import fs from "fs";
 import usuariosRutas from "./routes/usuariosRutas.js";
 import { conectarDB } from "./db/db.js";
 import './tasks/cronTasks.js';
@@ -28,10 +30,17 @@ app.use("/api", usuariosRutas);
 // Para Vercel (manejo de solicitudes como función)
 export default app;
 
-// Para ejecución local en localhost:3000
+// Para ejecución local con HTTPS en localhost:3000
 if (process.env.NODE_ENV !== "production") {
     const PORT = 3000;
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+
+    // Cargar certificados autofirmados para desarrollo
+    const options = {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+    };
+
+    https.createServer(options, app).listen(PORT, () => {
+        console.log(`Servidor corriendo en https://localhost:${PORT}`);
     });
 }
